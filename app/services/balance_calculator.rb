d5c64af02fad4f4c550a9156276d2054336ac51c
kristinadd <kristina@productboard.com>
2025-11-18 20:41:11 +0200

    Calculate balance

--- app/services/balance_calculator.rb
+++ app/services/balance_calculator.rb
@@ -5,9 +5,11 @@ class BalanceCalculator
     balance_config["balance_definitions"].keys
   end
 
-  def self.calculate(balance_name)
-    balance_definition = get_balance_definition(balance_name)
-    addresses = get_addresses(balance_name)
+  def self.calculate(balance_name:, account_id:)
+    entries = get_entries(balance_name:, account_id:)
+    balance = entries.sum(&:amount)
+
+    balance
   end
 
   private
@@ -25,8 +27,44 @@ class BalanceCalculator
   def self.get_addresses(balance_name)
     addresses_config = YAML.load_file(Rails.root.join("config/addresses.yml"))
 
-    addresses_config["addresses"].select do |address_key, address_data|
+    addresses = addresses_config["addresses"].select do |address_key, address_data|
       address_data["balance_definitions"]&.key?(balance_name)
     end
+
+    addresses
+  end
+
+  def self.handle_addresses(balance_name)
+    addresses = get_addresses(balance_name)
+
+    address_data = []
+
+    addresses.each do |address_key, address_value|
+      namespace = address_key.split(":")[0]
+      name = address_key.split(":")[1]
+
+      address_data << { namespace:, name: }
+    end
+
+    address_data
+  end
+
+  def self.get_entries(balance_name:, account_id:)
+    address_data = get_addresses(balance_name)
+
+    # balance_definition = get_balance_definition(balance_name)
+    # time_axis = balance_definition["time_axis"]
+
+    entries = []
+
+    address_data.each do |address_key, address_value|
+      namespace = address_key.split(":")[0]
+      name = address_key.split(":")[1]
+
+      entries << Entry.where(namespace:, name:, account_id:)
+                      .where.not(committed_at: nil).to_a # TODO: fix time_axis to be dynamic (commited_at or reporting_at), not hardcoded
+    end
+
+    entries.flatten
   end
 end

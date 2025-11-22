ac978e791372e7d49a0faab2055c45a8cdd8208c
kristinadd <kristina@productboard.com>
2025-11-22 17:45:54 +0200

    Group happy path examples

--- spec/api/v1/balances_controller_spec.rb
+++ spec/api/v1/balances_controller_spec.rb
@@ -2,28 +2,18 @@ require "rails_helper"
 
 RSpec.describe "API::V1::BalancesController", type: :request do
   describe "GET /api/v1/balances/available" do
-    it "returns list of available balance names" do
+    it "returns a list of available balance names" do
       get "/api/v1/balances/available"
 
       expect(response).to have_http_status(:ok)
+      expect(response.content_type).to include("application/json")
 
       json_response = JSON.parse(response.body)
       expect(json_response).to have_key("balances")
-      expect(json_response["balances"]).to be_an(Array)
-      expect(json_response["balances"]).to include("customer_facing_balance")
-      expect(json_response["balances"]).to include("interest_chargeable_balance")
-    end
-
-    it "returns JSON format" do
-      get "/api/v1/balances/available"
-
-      expect(response.content_type).to include("application/json")
-    end
-
-    it "returns successful status code" do
-      get "/api/v1/balances/available"
-
-      expect(response).to have_http_status(:ok)
+      expect(json_response["balances"]).to match_array([
+        "customer_facing_balance",
+        "interest_chargeable_balance"
+      ])
     end
   end
 end

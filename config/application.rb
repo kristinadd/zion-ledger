4df08308856a5c77141b184e6d55c5dd66aecba3
kristinadd <kristina@productboard.com>
2025-11-29 17:13:02 +0200

    Restrict direct access to ledger

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,10 @@ module ZionLedger
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Add Envoy verification middleware to block direct access
+    # Only requests coming through Envoy sidecar will be allowed
+    require_relative "../app/middleware/envoy_verification_middleware"
+    config.middleware.use EnvoyVerificationMiddleware
   end
 end
